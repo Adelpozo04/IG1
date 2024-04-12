332bceb1bcae7ed01e51f1e57a99a36758ba904d
Adelpozo04 <addelp01@ucm.es>
2024-04-12 16:30:31 +0200

    62 hecho

--- ProyectosIG_x64_VS2022/ProyectosIG_x64_VS2022/IG1App/Mesh.cpp
+++ ProyectosIG_x64_VS2022/ProyectosIG_x64_VS2022/IG1App/Mesh.cpp
@@ -480,3 +480,23 @@ Mesh* Mesh::generateWingAdvanceTie(GLdouble w, GLdouble h, GLdouble f) {
 	
 	return mesh;
 }
+
+//INDEXMESH
+
+void IndexMesh::render() const {
+	// Comandos OpenGL para enviar datos de arrays a GPU
+	// Nuevos comandos para la tabla de índices
+		if (vIndices != nullptr) {
+			glEnableClientState(GL_INDEX_ARRAY);
+			glIndexPointer(GL_UNSIGNED_INT, 0, vIndices);
+		}
+	// Comandos OpenGL para deshabilitar datos enviados
+	// Nuevo comando para la tabla de índices :
+		glDisableClientState(GL_INDEX_ARRAY);
+}
+
+// Comando para renderizar la malla indexada enviada
+void IndexMesh::draw() const {
+	glDrawElements(mPrimitive, nNumIndices,
+		GL_UNSIGNED_INT, vIndices);
+}
