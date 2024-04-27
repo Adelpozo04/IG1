be5c97cdca9289ea58bbec573910cb23aa7853cc
samuel <samuga05@ucm.es>
2024-04-27 18:17:08 +0200

    clase light

--- ProyectosIG_x64_VS2022/ProyectosIG_x64_VS2022/IG1App/Ilumination.cpp
+++ ProyectosIG_x64_VS2022/ProyectosIG_x64_VS2022/IG1App/Ilumination.cpp
@@ -39,3 +39,22 @@ void Material::setGold()
 }
 
 #pragma endregion
+
+GLuint Light::cont = 0;
+
+Light::Light()
+{
+	if (cont < GL_MAX_LIGHTS) {
+		id = GL_LIGHT0 + cont;
+		++cont;
+		glEnable(id);
+	}
+
+}
+
+void Light::uploadL() {
+	// Transfiere las características de la luz a la GPU
+	glLightfv(id, GL_AMBIENT, value_ptr(ambient));
+	glLightfv(id, GL_DIFFUSE, value_ptr(diffuse));
+	glLightfv(id, GL_SPECULAR, value_ptr(specular));
+}
--- ProyectosIG_x64_VS2022/ProyectosIG_x64_VS2022/IG1App/Ilumination.h
+++ ProyectosIG_x64_VS2022/ProyectosIG_x64_VS2022/IG1App/Ilumination.h
@@ -26,3 +26,44 @@ public:
 };
 
 
+class Light { // Abstract class
+protected:
+	static GLuint cont; // Atributo para poder generar un nuevo id cada vez
+	GLuint id = GL_LIGHT0 + GL_MAX_LIGHTS; // Primer id no válido
+	// Atributos lumínicos y geométrico de la fuente de luz
+	glm::fvec4 ambient = { 0.1 , 0.1 , 0.1 , 1 };
+	glm::fvec4 diffuse = { 0.5 , 0.5 , 0.5 , 1 };
+	glm::fvec4 specular = { 0.5 , 0.5 , 0.5 , 1 };
+	glm::fvec4 posDir = { 0 , 0 , 1 , 0 };
+public:
+	Light();
+	virtual ~Light() { disable(); }
+	void uploadL();
+
+
+	// Método abstracto
+	virtual void upload(glm::dmat4 const& modelViewMat) const = 0;
+	
+
+	void disable() { if (id < GL_LIGHT0 + GL_MAX_LIGHTS) glDisable(id); };
+	void enable() { if (id < GL_LIGHT0 + GL_MAX_LIGHTS) glEnable(id); };
+
+	// setters para cambiar el valor de los atributos lumínicos
+	void setAmb(glm::fvec4 amb) {
+		ambient = amb; uploadL();
+	};
+
+	void setDiff(glm::fvec4 diff) {
+		diffuse = diff; uploadL();
+	};
+
+	void setSpec(glm::fvec4 spec) {
+		specular = spec; uploadL();
+	};
+
+	//set dir
+	void setPosDir(glm::fvec4 pDir) {
+		posDir = pDir; uploadL();
+	};
+};
+
